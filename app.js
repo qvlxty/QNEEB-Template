c5f4211bd1c641696c42581c2bad4f4f2a8cfe0d
qvlxty <qvlxty@MacBook-Air-qvlxty.local>
2017-07-09 16:33:08 +0700

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var router = require('./router');
+
+//Router Setting
+app.use('/',router);
+//CSS/JS/ETC
+app.use('/css',express.static('views/css'));
+app.use('/fonts',express.static('views/fonts'));
+app.use('/js',express.static('views/js'));
+//VIEW ENGINE
+app.set('view engine','ejs');
+//For POST request 
+router.use(require('body-parser').urlencoded({ extended: true }));
+
+//LAUNCH
+app.listen(3000,()=>{console.log('Server running...');});
